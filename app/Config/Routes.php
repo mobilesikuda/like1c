94b4b58610924ef08b9781c10fda585d35378075
sikudamobile <ya.sikuda@yandex.ru>
2025-01-22 15:58:13 +0300

    restore delete

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,10 +9,11 @@ use App\Controllers\CatalogController;
 $routes->get('/', 'Home::index');
 $routes->get('catalogs', [CatalogController::class, 'index']);
 $routes->post('catalogs/update_view', [CatalogController::class, 'update_view']);
-//$routes->get('catalogs/update_view', [CatalogController::class, 'index']);
+
 $routes->post('catalogs', [CatalogController::class, 'update']);  
 $routes->get('catalogs/new', [CatalogController::class, 'new']); 
 $routes->get('catalogs/(:num)', [CatalogController::class, 'edit']); 
+//$routes->post('catalogs/delete', [CatalogController::class, 'update']);
 
 $routes->get('api/catalogs', [CatalogController::class, 'api']);
 $routes->get('api/catalogs/(:num)', [CatalogController::class, 'api']);  
--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
     <link rel="stylesheet" href="/sparkcss/spark.css">
-
 </head>
 <body>
 
