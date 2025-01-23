80d56e8d11ac23b07b8b89c54b1b89f693bbdad8
Sergey Kudashkin <ya.sikuda@yandex.ru>
2025-01-23 22:39:08 +0300

    megre main

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,7 +13,7 @@ $routes->post('catalogs/update_view', [CatalogController::class, 'update_view'])
 $routes->post('catalogs', [CatalogController::class, 'update']);  
 $routes->get('catalogs/new', [CatalogController::class, 'new']); 
 $routes->get('catalogs/(:num)', [CatalogController::class, 'edit']); 
-//$routes->post('catalogs/delete', [CatalogController::class, 'update']);
 
 $routes->get('api/catalogs', [CatalogController::class, 'api']);
 $routes->get('api/catalogs/(:num)', [CatalogController::class, 'api']);  
+
